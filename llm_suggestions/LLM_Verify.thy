ac0238007a2edb83544abff6420603104f8aa01f
Maximilian Schaeffeler <maximilian.schaffeler@kcl.ac.uk>
2026-06-03 19:53:13 +0100

    feat(isabelle-llm): add LLM-suggestions jEdit-plugin

---
+++ llm_suggestions/LLM_Verify.thy
@@ -0,0 +1,9 @@
+theory LLM_Verify
+  imports Main
+begin
+
+(* Reuse the existing goal-state + MePo relevant-facts extractor. *)
+ML_file \<open>proof_context_exporter.ML\<close>
+ML_file \<open>llm_verify.ML\<close>
+
+end
